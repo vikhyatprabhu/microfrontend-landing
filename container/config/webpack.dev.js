9e7238039a78d39ae19f0fb2f454f36dbf8928c1
vikhyat.prabhu <vikhyat.prabhu@gmail.com>
2021-02-13 12:25:17 +0530

    More changes for building and rename github action

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json')
@@ -22,9 +22,7 @@ const devConfig ={
            shared: packageJson['dependencies']
 
         }),
-        new HtmlWebpackPlugin({
-            template : './public/index.html'
-        })
+       
     ]
 }
 
